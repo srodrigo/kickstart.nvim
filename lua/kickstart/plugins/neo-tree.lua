756bd832ca9f9edd9832c8e171652c632ec85013
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-07 19:27:11 +0100

    feat(tree): configure tree explorer

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -28,10 +28,65 @@ return {
     },
   },
   opts = {
+    sources = { 'filesystem', 'buffers', 'git_status' },
+    open_files_do_not_replace_types = { 'terminal', 'qf', 'Outline' },
     filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
       window = {
         mappings = {
           ['<leader>e'] = 'close_window',
+          ['<space>'] = 'none',
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['Y'] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg('+', path, 'c')
+            end,
+            desc = 'Copy Path to Clipboard',
+          },
+          ['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } },
+          ['O'] = {
+            function(state)
+              local uri = state.tree:get_node().path
+              local cmd
+              if vim.fn.has 'win32' == 1 then
+                cmd = { 'explorer', uri }
+              elseif vim.fn.has 'macunix' == 1 then
+                cmd = { 'open', uri }
+              else
+                if vim.fn.executable 'xdg-open' == 1 then
+                  cmd = { 'xdg-open', uri }
+                elseif vim.fn.executable 'wslview' == 1 then
+                  cmd = { 'wslview', uri }
+                else
+                  cmd = { 'open', uri }
+                end
+              end
+
+              local ret = vim.fn.jobstart(cmd, { detach = true })
+              if ret <= 0 then
+                local msg = {
+                  'Failed to open uri',
+                  ret,
+                  vim.inspect(cmd),
+                }
+                vim.notify(table.concat(msg, '\n'), vim.log.levels.ERROR)
+              end
+            end,
+            desc = 'Open with System File Brower',
+          },
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = '',
+          expander_expanded = '',
+          expander_highlight = 'NeoTreeExpander',
         },
       },
     },
