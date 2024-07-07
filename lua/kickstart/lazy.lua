25c85cfd79b560fa14d20f63ab94bc0d11d3f4a2
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-07 10:03:00 +0100

    feat(surround): add vim-surround

--- lua/kickstart/lazy.lua
+++ lua/kickstart/lazy.lua
@@ -24,7 +24,9 @@ require('lazy').setup({
   ----------------
 
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',   -- Detect tabstop and shiftwidth automatically
+
+  'tpope/vim-surround', -- Provide surroundings for parentheses, brackets, quotes and others
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
