ba0db2093ca681e920a9a12930161eeae124457a
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-07 11:43:22 +0100

    feat(autoformat): disable autoformat autocommand
    The conform handles this now.

--- lua/kickstart/config/autocommands.lua
+++ lua/kickstart/config/autocommands.lua
@@ -13,14 +13,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Format on save
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('lsp', { clear = true }),
-  callback = function(args)
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      buffer = args.buf,
-      callback = function()
-        vim.lsp.buf.format { async = false, id = args.data.client_id }
-      end,
-    })
-  end,
-})
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   group = vim.api.nvim_create_augroup('lsp', { clear = true }),
+--   callback = function(args)
+--     vim.api.nvim_create_autocmd('BufWritePre', {
+--       buffer = args.buf,
+--       callback = function()
+--         vim.lsp.buf.format { async = false, id = args.data.client_id }
+--       end,
+--     })
+--   end,
+-- })
