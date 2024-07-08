0bb3e1bb2301e918f5adb584ef9d4f27c44d1150
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-08 20:02:59 +0100

    feat(navigation): add alternative keymaps to resize windows

--- lua/kickstart/config/keymaps.lua
+++ lua/kickstart/config/keymaps.lua
@@ -74,6 +74,10 @@ map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
 map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
 map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
 map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+map('n', '<M-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+map('n', '<M-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+map('n', '<M-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+map('n', '<M-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
 
 -- Buffers
 map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
