25463b998a14a867540a66ff00bf0eba92c1c13e
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-10 13:01:14 +0100

    fix(search): remove conflicting symbols mapping

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -112,9 +112,7 @@ return {
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>sS', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace [S]ymbols')
-
-          map('<leader>ss', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Buffer [s]ymbols')
+          -- map('<leader>sS', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace [S]ymbols')
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
