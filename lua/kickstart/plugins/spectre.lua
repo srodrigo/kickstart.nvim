f1489c8ba68a46f676f46f817b4a885b8b7cd997
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-13 10:26:25 +0100

    feat(search): add replace in buffer/files shorter keymap

--- lua/kickstart/plugins/spectre.lua
+++ lua/kickstart/plugins/spectre.lua
@@ -6,5 +6,7 @@ return {
   -- stylua: ignore
   keys = {
     { "<leader>sR", function() require("spectre").open() end, desc = "Search & [R]eplace in Files (Spectre)" },
+    { "<leader>r", function() require("spectre").open({ path = vim.fn.expand('%') }) end, desc = "[r]eplace in Buffer (Spectre)" },
+    { "<leader>R", function() require("spectre").open() end, desc = "[R]eplace in Files (Spectre)" },
   },
 }
