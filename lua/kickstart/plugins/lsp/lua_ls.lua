6fc31c318915ef7eb03aae3d7bb0d77c50bf525a
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-14 12:23:33 +0100

    feat(lua): add support for love2d

--- lua/kickstart/plugins/lsp/lua_ls.lua
+++ lua/kickstart/plugins/lsp/lua_ls.lua
@@ -9,6 +9,11 @@ return {
       },
       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
+      workspace = {
+        library = {
+          '${3rd}/love2d/library',
+        },
+      },
     },
   },
 }
