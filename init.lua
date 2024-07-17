3ade183b7c8f6cfae6895d8c6fb1af8f4dbee67c
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-07-17 20:49:53 +0100

    feat(profilling): add profilling

--- init.lua
+++ init.lua
@@ -1,4 +1,30 @@
 require 'kickstart'
 
+local should_profile = os.getenv 'NVIM_PROFILE'
+if should_profile then
+  require('profile').instrument_autocmds()
+  if should_profile:lower():match '^start' then
+    require('profile').start '*'
+  else
+    require('profile').instrument '*'
+  end
+end
+
+local function toggle_profile()
+  local prof = require 'profile'
+  if prof.is_recording() then
+    prof.stop()
+    vim.ui.input({ prompt = 'Save profile to:', completion = 'file', default = 'profile.json' }, function(filename)
+      if filename then
+        prof.export(filename)
+        vim.notify(string.format('Wrote %s', filename))
+      end
+    end)
+  else
+    prof.start '*'
+  end
+end
+vim.keymap.set('', '<f1>', toggle_profile)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
