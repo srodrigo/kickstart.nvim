432fdf872cb9ec61a69574df44cec8a51bff8533
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-08-14 07:26:01 +0100

    feat(replace): provide selected text

--- lua/kickstart/plugins/spectre.lua
+++ lua/kickstart/plugins/spectre.lua
@@ -1,3 +1,9 @@
+local function get_visual_selection()
+  -- Yank current visual selection into the 'v' register
+  vim.cmd 'noau normal! "vy"'
+  return vim.fn.getreg 'v'
+end
+
 return {
   'nvim-pack/nvim-spectre',
   build = false,
@@ -6,7 +12,9 @@ return {
   -- stylua: ignore
   keys = {
     { "<leader>sR", function() require("spectre").open() end, desc = "Search & [R]eplace in Files (Spectre)" },
-    { "<leader>r", function() require("spectre").open({ path = vim.fn.expand('%') }) end, desc = "[r]eplace in Buffer (Spectre)" },
-    { "<leader>R", function() require("spectre").open() end, desc = "[R]eplace in Files (Spectre)" },
+    { "<leader>r", function() require("spectre").open({ path = vim.fn.expand('%') }) end, desc = "[r]eplace in Buffer (Spectre)", mode = 'n' },
+    { "<leader>r", function() require("spectre").open({ path = vim.fn.expand('%'), search_text=get_visual_selection() }) end, desc = "[r]eplace in Buffer (Spectre)", mode = 'v' },
+    { "<leader>R", function() require("spectre").open() end, desc = "[R]eplace in Files (Spectre)", mode = 'n' },
+    { "<leader>R", function() require("spectre").open({ search_text=get_visual_selection() }) end, desc = "[R]eplace in Files (Spectre)", mode = 'v' },
   },
 }
