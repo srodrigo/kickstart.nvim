baaaffbc942afb250311f3a6691162473772e987
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-12-18 13:20:11 +0000

    refactor(colorscheme): remove tokyonight config

--- lua/kickstart/plugins/colorschemes.lua
+++ lua/kickstart/plugins/colorschemes.lua
@@ -8,21 +8,6 @@ local nordBackground = '#2D303C'
 vim.cmd.hi 'Comment gui=none'
 
 local all_colorschemes = {
-  tokyonight = { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = priority,
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- Default: 'tokyonight-night'
-      vim.cmd.colorscheme 'tokyonight-moon'
-    end,
-  },
   nordbones = {
     'zenbones-theme/zenbones.nvim',
     dependencies = {
