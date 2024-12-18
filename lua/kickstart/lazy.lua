4b8db2e03a678d09a7de347bfea466e20865d755
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-12-18 13:19:03 +0000

    refactor(lazy): clean todo-comments up

--- lua/kickstart/lazy.lua
+++ lua/kickstart/lazy.lua
@@ -103,8 +103,6 @@ require('lazy').setup({
 
   -- require 'kickstart.plugins.rainbow-parentheses',
 
-  -- require 'kickstart.plugins.todo-comments',
-
   require 'kickstart.plugins.copilot-chat',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
